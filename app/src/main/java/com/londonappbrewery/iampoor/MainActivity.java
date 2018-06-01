fc01ea6c5a75d906d7a4534562ca7dc85f949ca2
pjs423 <asvr90gm2@gmail.com>
2018-06-01 09:45:31 +0100

    Change textview textsize Changes to gradle to overcome error

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -3,6 +3,7 @@ package com.londonappbrewery.iampoor;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.appcompat.*;
 
 public class MainActivity extends AppCompatActivity {
 
